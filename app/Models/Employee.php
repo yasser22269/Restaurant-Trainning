70fdc151783b572f0fffcb3ac71f67bd682cfd2d
Yasser Abd Elghany <yasser.m22291@gmail.com>
2022-12-10 21:52:15 +0200

    Update Employee.php

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -4,23 +4,15 @@ namespace App\Models;
 
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-use Laravel\Sanctum\HasApiTokens;
-use Illuminate\Auth\Authenticatable;
-use App\Models\TimeEmp;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Sanctum\HasApiTokens;
 
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
-
-
-class Employee extends Model implements AuthenticatableContract
+class Employee extends Authenticatable
 {
 
     use Notifiable;
-    use Authenticatable;
-
     use HasFactory;
     use HasApiTokens;
 
